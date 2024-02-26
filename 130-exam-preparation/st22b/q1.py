be9e550684fcf9b6c5404079d7e8a1dca196cc30
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2024-02-26 15:41:52 +0100

    cleanup

--- 130-exam-preparation/st22b/q1.py
+++ 130-exam-preparation/st22b/q1.py
@@ -1,29 +1,20 @@
 import math
 
-# All pensions will get 5% increase
-# The minimal pension is set to 450. If after the 5% increase the pension is still smaller than
-# 450, then it is set to 450.
-# The maximal pension is 2500. Pensions that become greater than that after the increase
-# are set to 2500.
-# The pension is always rounded up to a whole number.
-# Write a python program which asks the user for his/her current pension and prints the new one. The
-# following are example dialogs with the user.
+
+current = float(input("Current pension?"))
+actual = 1.05 * current
+if actual < 450:
+    actual = 450
+if actual > 2500:
+    actual = 2500
+actual=math.ceil(actual)
+actual=int(actual)
+print(actual)
+    
+# What if you don't want to use math.ceil?
 
 def my_ceil(x):
     if int(x) == x:
         return int(x)
     else:
         return int(x+1)
-
-my_ceil(2.1)
-
-# current = float(input("Current pension?"))
-# actual = 1.05 * current
-# if actual < 450:
-#     actual = 450
-# if actual > 2500:
-#     actual = 2500
-# actual=math.ceil(actual)
-# actual=int(actual)
-# print(actual)
-    
